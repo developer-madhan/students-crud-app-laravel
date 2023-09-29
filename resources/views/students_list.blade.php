3f2bef34524dbf40f74fd2858ce44f56b130d8ac
atpcs011 <atpcs011@outlook.com>
2023-09-29 18:03:08 +0530

    finished delete function and try show little

--- resources/views/students_list.blade.php
+++ resources/views/students_list.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @extends('layouts.navbar')
 @section('content')
-@if(session()->get('message'))
-<div class="mt-3 alert alert-success">
-    {{session()->get('message')}}
-</div>
-@endif
+    @if (session()->get('message'))
+        <div class="mt-3 alert alert-success">
+            {{ session()->get('message') }}
+        </div>
+    @endif
     <h1>Students Records</h1>
     <table class="table table-bordered border-primary table-hover">
         <thead>
@@ -23,10 +23,22 @@
                     <td>{{ $student->name }}</td>
                     <td>{{ $student->s_class }}</td>
                     <td>
-                        <a href="{{route('students.show',$student->id)}}"><i class="fa-regular fa-eye fa-shake fa-xl" style="color: green;"></i></a>&nbsp;&nbsp;&nbsp;
-                        <a href="{{route('students.edit',$student->id)}}"><i class="fa-solid fa-pen-to-square fa-bounce fa-xl" style="color: blue"></i></a>&nbsp;&nbsp;&nbsp;
-                        <a href="{{route('students.destroy',$student->id)}}"><i class="fa-solid fa-person-circle-minus fa-fade fa-xl" style="color: red"></i></a>
+                        <a href="{{ route('students.show', $student->id) }}" style="margin-right: 10px;"><i
+                                class="fa-regular fa-eye fa-shake fa-xl" style="color: green;"></i></a>
+                        <a href="{{ route('students.edit', $student->id) }}" style="margin-right: 10px;"><i
+                                class="fa-solid fa-pen-to-square fa-bounce fa-xl" style="color: blue"></i></a>
+                        <form action="{{ route('students.destroy', $student->id) }}" method="post"
+                            style="display: inline-block;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                style="border: none; background: transparent; outline: none; padding: 0; margin: 0;"
+                                onclick="confirm('Are you sure you want to delete this item?');"><i
+                                    class="fa-solid fa-person-circle-minus fa-fade fa-xl" style="color: red"></i></button>
+
+                        </form>
                     </td>
+
 
                 </tr>
             @endforeach
@@ -35,4 +47,27 @@
 
     </table>
 
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Name: <p>{{$student->name}}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
